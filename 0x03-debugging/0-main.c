eb2c9083972105ae0ed2711f482e63309f8db223
ImadPro69 <imadeddine0.0es@gmail.com>
2023-03-23 14:06:42 -0700

    help

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,17 +7,10 @@
 
 int main(void)
 {
-<<<<<<< HEAD
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
 
 	return (0);
-=======
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
->>>>>>> c90979abbcf3cfe6e66bd87303287b517d3f9098
 }
