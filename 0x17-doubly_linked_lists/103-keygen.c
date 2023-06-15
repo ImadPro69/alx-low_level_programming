b5ba4d48863b4a5a264afb3981c43ad08a72d6a1
Imadpro69 <imadeddine0.0es@gmail.com>
2023-06-15 13:31:43 -0700

    task 11

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,37 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 
-char *generate_key(const char *username)
+int f4(char *usrn, int len)
 {
-    size_t i, username_len = strlen(username);
-    char *key = malloc((username_len + 1) * sizeof(char));
+	int ch;
+	int vch;
+	unsigned int rand_num;
 
-    for (i = 0; i < username_len; i++) {
-        key[i] = username[i] ^ (char)i;
-    }
+	ch = *usrn;
+	vch = 0;
 
-    key[i] = '\0';
+	while (vch < len)
+	{
+		if (ch < usrn[vch])
+			ch = usrn[vch];
+		vch += 1;
+	}
 
-    return key;
+	srand(ch ^ 14);
+	rand_num = rand();
+
+	return (rand_num & 63);
 }
 
-int main(int argc, char *argv[])
+int f5(char *usrn, int len)
 {
-    const char *username;
-    char *key;
+	int ch;
+	int vch;
 
-    if (argc != 2) {
-        printf("Usage: %s <username>\n", argv[0]);
-        return 1;
-    }
+	ch = vch = 0;
 
-    username = argv[1];
-    key = generate_key(username);
+	while (vch < len)
+	{
+		ch = ch + usrn[vch] * usrn[vch];
+		vch += 1;
+	}
 
-    printf("%s\n", key);
+	return (((unsigned int)ch ^ 239) & 63);
+}
 
-    free(key);
+int f6(char *usrn)
+{
+	int ch;
+	int vch;
 
-    return 0;
+	ch = vch = 0;
+
+	while (vch < *usrn)
+	{
+		ch = rand();
+		vch += 1;
+	}
+
+	return (((unsigned int)ch ^ 229) & 63);
+}
+
+int main(int argc, char **argv)
+{
+	char keygen[7];
+	int len, ch, vch;
+	long alph[] = {
+		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+		0x723161513346655a, 0x6b756f494b646850
+	};
+
+	for (len = 0; argv[1][len]; len++)
+		;
+
+	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
+
+	ch = vch = 0;
+	while (vch < len)
+	{
+		ch = ch + argv[1][vch];
+		vch = vch + 1;
+	}
+	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
+
+	ch = 1;
+	vch = 0;
+	while (vch < len)
+	{
+		ch = argv[1][vch] * ch;
+		vch = vch + 1;
+	}
+	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
+
+	keygen[3] = ((char *)alph)[f4(argv[1], len)];
+
+	keygen[4] = ((char *)alph)[f5(argv[1], len)];
+
+	keygen[5] = ((char *)alph)[f6(argv[1])];
+	keygen[6] = '\0';
+
+	for (ch = 0; keygen[ch]; ch++)
+		printf("%c", keygen[ch]);
+
+	return 0;
 }
