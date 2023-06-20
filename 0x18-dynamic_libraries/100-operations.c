a17b6ada976044835b40687fecbd7c217a2f389f
Imadpro69 <imadeddine0.0es@gmail.com>
2023-06-19 17:17:30 -0700

    task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,20 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        /* Handle division by zero error */
+        return 0;
+    }
+}
