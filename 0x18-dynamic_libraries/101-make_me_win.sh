1b093a6aec33a50861bc17b4389d62d833e5656c
Imadpro69 <imadeddine0.0es@gmail.com>
2023-06-19 17:28:39 -0700

    task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+
+# Set the LD_PRELOAD environment variable to the path of the shared library
+export LD_PRELOAD=./override.so
+
+# Run the Giga Millions program with your chosen numbers
+./gm 9 8 10 24 75 9
+
+
