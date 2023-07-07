c86dc245f28fa47fc679d07953d498c8ffbd7ebe
Imadpro69 <imadeddine0.0es@gmail.com>
2023-07-07 16:41:28 -0700

    all mandatory

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - DJB2 hash function
+ * @str: The input string to be hashed
+ *
+ * Return: The calculated hash value
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash = 5381;
+    int c;
+
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
+    return hash;
+}
+
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - Calculates the index of a key in the hash table array
+ * @key: The key to calculate the index for
+ * @size: The size of the array of the hash table
+ *
+ * Return: The index at which the key/value pair should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int hash_value;
+    unsigned long int index;
+
+    hash_value = hash_djb2(key);  /* Use the hash_djb2 function to get the hash value */
+    index = hash_value % size;    /* Calculate the index using modulo operator */
+
+    return index;
+}
+
